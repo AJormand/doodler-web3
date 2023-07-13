8e20c75099538afc4a9c3532f2ab8c5d8fd442b0
Aro <aro.jormand@gmail.com>
2023-07-13 16:18:46 +0200

    storing abi and addresses

--- deploy/99-update-frontend.js
+++ deploy/99-update-frontend.js
@@ -1,51 +1,51 @@
 const { ethers, network } = require("hardhat");
 const fs = require("fs");
+//using dotenv as env.local works only inside app folder in nextjs
 require("dotenv").config();
 
-const frontEndContractFile =
-  "../nft-marketplace-client/constants/networkMapping.json";
-const frontendAbiLocation = "../nft-marketplace-client/constants/";
+const frontEndContractFile = "../doodler-web3/constants/networkMapping.json";
+const frontendAbiLocation = "../doodler-web3/constants/";
 
 module.exports = async function () {
+  console.log(process.env.UPDATE_FRONT_END);
+
   if (process.env.UPDATE_FRONT_END) {
-    console.log("updating frontend");
     await updateContractAddress();
     await updateAbi();
   }
 };
 
 async function updateAbi() {
-  const nftMarketplace = await ethers.getContract("NftMarketplace");
-  fs.writeFileSync(
-    `${frontendAbiLocation}NftMarketplace.json`,
-    nftMarketplace.interface.format(ethers.utils.FormatTypes.json)
-  );
+  const betContract = await ethers.getContract("BetContract");
 
-  const basicNft = await ethers.getContract("BasicNft");
   fs.writeFileSync(
-    `${frontendAbiLocation}BasicNft.json`,
-    basicNft.interface.format(ethers.utils.FormatTypes.json)
+    `${frontendAbiLocation}BetContract.json`,
+    betContract.interface.formatJson()
   );
 }
 
 async function updateContractAddress() {
-  const nftMarketplace = await ethers.getContract("NftMarketplace");
+  const betContract = await ethers.getContract("BetContract");
+  console.log(betContract.target);
   const chainId = network.config.chainId.toString();
+  console.log(chainId);
   const contractAddresses = JSON.parse(
     fs.readFileSync(frontEndContractFile, "utf8")
   );
+  console.log(contractAddresses);
+
+  console.log(chainId in contractAddresses);
 
   if (chainId in contractAddresses) {
     if (
-      !contractAddresses[chainId]["NftMarketplace"].includes(
-        nftMarketplace.address
-      )
+      !contractAddresses[chainId]["BetContract"].includes(betContract.target)
     ) {
-      contractAddresses[chainId]["NftMarketplace"].push(nftMarketplace.address);
+      contractAddresses[chainId]["BetContract"].push(betContract.target);
     }
   } else {
+    console.log(betContract.target);
     contractAddresses[chainId] = {
-      NftMarketplace: [nftMarketplace.address],
+      BetContract: [betContract.target],
     };
   }
 
